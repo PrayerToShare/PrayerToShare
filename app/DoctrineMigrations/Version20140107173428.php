23f597888b3d1111586df19b196d0662cfacde62
Jonathon Suggs <jsuggs@murmp.com>
2014-01-07 17:37:20 -0600

    Start of a migration

---
+++ app/DoctrineMigrations/Version20140107173428.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20140107173428 extends AbstractMigration
+{
+    const PRAYER_REQUESTS_SQL =<<<SQL
+CREATE TABLE prayer_requests (
+    id INT AUTO_INCREMENT NOT NULL
+  , user_id INT DEFAULT NULL
+  , text VARCHAR(255) NOT NULL
+  , INDEX IDX_PRAYER_REQUESTS_USER_ID (user_id)
+  , PRIMARY KEY(id)
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB
+SQL;
+
+    public function up(Schema $schema)
+    {
+        $this->addSql(self::PRAYER_REQUESTS_SQL);
+        $this->addSql("ALTER TABLE prayer_requests ADD CONSTRAINT FK_PRAYER_REQUESTS_REF_USERS_USER_ID FOREIGN KEY (user_id) REFERENCES users (id)");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->addSql("DROP TABLE prayer_requests");
+    }
+}
