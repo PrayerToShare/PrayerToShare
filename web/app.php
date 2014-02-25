a9d1eae73d08a10fa30d31f8a307ac2338245576
Jonathon Suggs <jsuggs@murmp.com>
2014-02-25 01:35:08 -0600

    Terminated

--- web/app.php
+++ web/app.php
@@ -1,12 +1,15 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 require_once __DIR__.'/../app/AppCache.php';
 
-use Symfony\Component\HttpFoundation\Request;
-
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
-$kernel->handle(Request::createFromGlobals())->send();
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
