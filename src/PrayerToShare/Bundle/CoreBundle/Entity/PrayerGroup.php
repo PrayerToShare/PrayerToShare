c9e1bd5456cd99e9b06265a9859eccc173983331
Jonathon Suggs <jsuggs@murmp.com>
2014-02-28 16:25:50 -0600

    Basic entity setup

--- src/PrayerToShare/Bundle/CoreBundle/Entity/PrayerGroup.php
+++ src/PrayerToShare/Bundle/CoreBundle/Entity/PrayerGroup.php
@@ -4,11 +4,14 @@ namespace PrayerToShare\Bundle\CoreBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serialize;
+use PrayerToShare\Bundle\MainBundle\Entity\Prayer;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="PrayerGroupRepository")
  * @ORM\Table(name="prayer_groups")
+ * @Serialize\ExclusionPolicy("all")
  */
 class PrayerGroup
 {
@@ -16,12 +19,14 @@ class PrayerGroup
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Serialize\Expose
      */
     protected $id;
 
     /**
      * @ORM\Column(type="string", length=64)
      * @Assert\NotBlank
+     * @Serialize\Expose
      */
     protected $name;
 
@@ -31,13 +36,20 @@ class PrayerGroup
     protected $members;
 
     /**
+     * @ORM\OneToMany(targetEntity="PrayerToShare\Bundle\MainBundle\Entity\Prayer", mappedBy="prayerGroup")
+     */
+    protected $prayers;
+
+    /**
      * @ORM\Column(type="boolean")
+     * @Serialize\Expose
      */
     protected $public;
 
     public function __construct()
     {
         $this->members = new ArrayCollection();
+        $this->prayers = new ArrayCollection();
     }
 
     public function getId()
@@ -79,6 +91,18 @@ class PrayerGroup
         return $this->getAdmins()->exists(function() use ($user) {
             return $member->getUser() == $user;
         });
+    }
+
+    public function addPrayer(Prayer $prayer)
+    {
+        if (!$this->prayers->contains($prayer)) {
+            $this->prayers->add($prayer);
+        }
+    }
+
+    public function getPrayers()
+    {
+        return $this->prayers;
     }
 
     public function setPublic($public)
