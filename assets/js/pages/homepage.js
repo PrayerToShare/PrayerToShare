8c3ac8b6a60fe0c2d7a3a2562577b92293a9dd34
Joel Nye <jnye@theopenskyproject.com>
2014-02-28 15:44:07 -0600

    added slide animation to homepage

--- assets/js/pages/homepage.js
+++ assets/js/pages/homepage.js
@@ -1,10 +1,30 @@
-require(['jquery', 'modules/Prayer'], function($, Prayer) {
+define(['jquery', 'domReady', 'modules/Prayer'], function($, domReady, Prayer) {
     var page = {
+        initFormSlide: function() {
+            $('.toggle-login').on('click', function(e) {
+                e.preventDefault();
+                $(this).parents('.slide-container').animate({
+                    'right': '-=1018px'
+                }, 500, function() {
+                    // when done - possibly initialize other events?
+                });
+            });
+            $('.toggle-register').on('click', function(e) {
+                e.preventDefault();
+                $(this).parents('.slide-container').animate({
+                    'right': '+=1018px'
+                }, 500, function() {
+                    // when done - possibly initialize other events?
+                });
+            });
+        },
+
         init: function() {
+        	domReady(function() {
+                page.initFormSlide();
+        	});
         }
     };
 
-    return {
-        init: page.init
-    };
+    init: page.init();
 });
