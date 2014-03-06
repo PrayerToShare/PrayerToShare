a86b506b0b7919e2b63f4783c8f2419dfbc58512
Jonathon Suggs <jsuggs@murmp.com>
2014-03-06 00:36:48 -0600

    First shot at restricting prayer feed

--- src/PrayerToShare/Bundle/MainBundle/Entity/PrayerRepository.php
+++ src/PrayerToShare/Bundle/MainBundle/Entity/PrayerRepository.php
@@ -7,10 +7,32 @@ use PrayerToShare\Bundle\CoreBundle\Entity\User;
 
 class PrayerRepository extends EntityRepository
 {
+    const NETWORK_PRAYER_DQL =<<<DQL
+SELECT p
+FROM PrayerToShare\Bundle\MainBundle\Entity\Prayer p
+WHERE
+    (
+        p.user = :user
+        OR
+        p.prayerGroup IN (
+            SELECT pg1
+            FROM PrayerToShare\Bundle\CoreBundle\Entity\PrayerGroup pg1
+            JOIN pg1.members m1
+            WHERE m1.user = :user
+        )
+    )
+    AND NOT EXISTS (
+        SELECT 1
+        FROM PrayerToShare\Bundle\MainBundle\Entity\ArchivedPrayer ap2
+        WHERE ap2.prayer = p
+        AND p.user = :user
+    )
+DQL;
+
     public function findNetworkPrayers(User $user)
     {
-        return $this->createQueryBuilder('p')
-            ->getQuery()
+        return $this->_em->createQuery(self::NETWORK_PRAYER_DQL)
+            ->setParameters(array('user' => $user))
             ->getResult();
     }
 }
