a1f9df41c03c8a0fe4041cc9c52f38e32a51180d
Jonathon Suggs <jsuggs@murmp.com>
2014-03-06 20:38:58 -0600

    Change groups route to singular

--- src/PrayerToShare/Bundle/MainBundle/Controller/GroupController.php
+++ src/PrayerToShare/Bundle/MainBundle/Controller/GroupController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * @Route("/groups")
+ * @Route("/group")
  */
 class GroupController extends BaseController
 {
