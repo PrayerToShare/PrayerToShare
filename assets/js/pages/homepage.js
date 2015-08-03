1e2118c8922c4a5bacfb996dae21e9818a84419e
Joel Nye <jnye@theopenskyproject.com>
2015-08-03 11:14:44 -0500

    updated design of site -- worked on home/login/register page

--- assets/js/pages/homepage.js
+++ assets/js/pages/homepage.js
@@ -1,27 +1,8 @@
 define(['jquery', 'domReady', 'modules/Prayer'], function($, domReady, Prayer) {
     var page = {
-        initFormSlide: function() {
-            $('.toggle-login').on('click', function(e) {
-                e.preventDefault();
-                $(this).parents('.slide-container').animate({
-                    'right': '-=1018px'
-                }, 500, function() {
-                    // when done - possibly initialize other events?
-                });
-            });
-            $('.toggle-register').on('click', function(e) {
-                e.preventDefault();
-                $(this).parents('.slide-container').animate({
-                    'right': '+=1018px'
-                }, 500, function() {
-                    // when done - possibly initialize other events?
-                });
-            });
-        },
-
-        init: function() {
+        init: function init() {
             domReady(function() {
-                page.initFormSlide();
+
             });
         }
     };
